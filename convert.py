860276bbfc6c8b232fdf620309f2a56683790ada
CannonLock <lockannon@gmail.com>
2021-12-19 20:53:59 -0600

    Add Github Action

---
+++ convert.py
@@ -0,0 +1,18 @@
+import csv
+import os
+import json
+
+def csv_to_json(path: str):
+
+  with open(path, "r") as fp:
+    csv_list = list(csv.DictReader(fp))
+
+  file_name = os.path.basename(path).split(".")[0]
+
+  with open(f"./{file_name}.json", "w") as fp:
+    fp.write(json.dump(csv_list, fp))
+
+def main():
+  csv_to_json("./benchmark.csv")
+
+main()
